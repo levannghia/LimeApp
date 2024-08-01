63421095bd10528e1f9b5c4acab2e44bc2b47e50
levannghia <levannghia.tdc2018@gmail.com>
2024-08-01 14:53:50 +0700

    Update

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,14 +1,31 @@
 import { View, Text } from 'react-native'
-import React from 'react'
-import Mapbox, { MapView } from '@rnmapbox/maps'
+import React, { useState } from 'react'
+import Mapbox, { Camera, MapView, StyleImport } from '@rnmapbox/maps'
 import { appInfo } from '../constants/appInfo'
 
 Mapbox.setAccessToken(appInfo.MAPBOX_ACCESS_TOKEN)
 
 const Map = () => {
-  return (
-    <MapView style={{flex: 1}} styleURL='mapbox://styles/mapbox/outdoors-v12'/>
-  )
+    const [lightPreset, setLightPreset] = useState('night');
+    const nextLightPreset = lightPreset === 'night' ? 'day' : 'night';
+    return (
+        <MapView style={{ flex: 1 }} styleURL='mapbox://styles/mapbox/outdoors-v12'>
+            <Camera
+                defaultSettings={{ centerCoordinate: [108.997015, 11.5653169] }}
+                centerCoordinate={[108.997015, 11.5653169]}
+                animationDuration={0}
+                zoomLevel={16}
+                pitch={33}
+            />
+            <StyleImport
+                id="basemap"
+                existing
+                config={{
+                    lightPreset: lightPreset,
+                }}
+            />
+        </MapView>
+    )
 }
 
 export default Map
