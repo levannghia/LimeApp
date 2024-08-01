f291d252b9d21a82ab9c6f1c24de1e970f49f43e
levannghia <levannghia.tdc2018@gmail.com>
2024-08-01 15:45:16 +0700

    Up

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,29 +1,31 @@
 import { View, Text } from 'react-native'
-import React, { useState } from 'react'
-import Mapbox, { Camera, MapView, StyleImport } from '@rnmapbox/maps'
+import React, { useEffect, useState } from 'react'
+import Mapbox, { Camera, MapView, StyleImport, LocationPuck } from '@rnmapbox/maps'
+import Geolocation from '@react-native-community/geolocation'
 import { appInfo } from '../constants/appInfo'
 
 Mapbox.setAccessToken(appInfo.MAPBOX_ACCESS_TOKEN)
 
 const Map = () => {
-    const [lightPreset, setLightPreset] = useState('night');
-    const nextLightPreset = lightPreset === 'night' ? 'day' : 'night';
+    const [currentLocation, setCurrentLocation] = useState({});
+
+    useEffect(() => {
+        Geolocation.getCurrentPosition(position => {
+            console.log(position);
+        })
+    }, []);
+    
     return (
         <MapView style={{ flex: 1 }} styleURL='mapbox://styles/mapbox/outdoors-v12'>
             <Camera
-                defaultSettings={{ centerCoordinate: [108.997015, 11.5653169] }}
-                centerCoordinate={[108.997015, 11.5653169]}
-                animationDuration={0}
-                zoomLevel={16}
-                pitch={33}
+                // defaultSettings={{ centerCoordinate: [108.997015, 11.5653169] }}
+                // centerCoordinate={[108.997015, 11.5653169]}
+                // animationDuration={0}
+                // zoomLevel={16}
+                // pitch={33}
+                followUserLocation
             />
-            <StyleImport
-                id="basemap"
-                existing
-                config={{
-                    lightPreset: lightPreset,
-                }}
-            />
+            <LocationPuck />
         </MapView>
     )
 }
